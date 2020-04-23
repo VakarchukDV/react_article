347955475188ee1179828c884cc64be0fff27a66
euxos <eugen.ostrozhinsky@gmail.com>
2020-04-23 11:07:26 +0300

    Updates

---
+++ precache-manifest.5511efb919f0574cc76685d1056f00e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401f5a0398f1d4e5e498bfda20e7b0f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26d1661a212078421fa6",
+    "url": "./static/js/2.b2110f2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b2110f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055a06b7a98c9faca079",
+    "url": "./static/js/main.ee474b68.chunk.js"
+  },
+  {
+    "revision": "9a40466c9e3fec559fa3",
+    "url": "./static/js/runtime-main.6a591e0b.js"
+  }
+]);
